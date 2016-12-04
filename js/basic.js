9fac14e5ac7ce0a5bbfa29dde5088e36251da3ae
annaesthe <an.sieradzka@gmail.com>
2016-12-04 10:19:55 +0100

    init

---
+++ js/basic.js
@@ -0,0 +1,23 @@
+
+var przyprostokatna1 = 6;
+var przyprostokatna2 = 7;
+
+var przeciwprostokatna = Math.sqrt(Math.pow(przyprostokatna1, 2) + Math.pow(przyprostokatna2, 2));
+
+console.log(przeciwprostokatna);
+
+var losowa = Math.round(Math.random()*10);
+console.log(losowa);
+
+var text = ''
+
+if (2 == 2) {
+    var test = 'warunek spełniony';
+}
+
+console.log(test);
+
+var i = 1;
+do (
+    document.write(Pętla do..while [i = " + i "])
+) while
